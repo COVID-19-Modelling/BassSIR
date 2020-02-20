ba69454d91c6781ff71a0b7777e0378d63c3c4b7
TimeWz667 <timewz667@gmail.com>
2020-02-20 00:28:01 +0000

    Add data

---
+++ R/data.R
@@ -0,0 +1,60 @@
+#' Confirmed cases, the recovered, and peopole who died from COVID-19
+#'
+#' A dataset containing the Confirmed cases, the recovered, and peopole who died from COVID-19
+#' by chinese province
+#'
+#'
+#' @format A list of data frames Province -> data.frame:
+#' \describe{
+#'   \item{Date}{Date of the entry}
+#'   \item{Confirmed}{Confirmed cases}
+#'   \item{Dead}{People who died from COVID-19}
+#'   \item{Cured}{People who recovered from COVID-19}
+#'   ...
+#' }
+#' @source \url{}
+"n_covid19"
+
+
+
+#' Prepare data for BassSIR modelling
+#'
+#' @param d source data.frame
+#' @param location identity of the data
+#' @param i_date index indicating date
+#' @param i_confirmed index indicating confirmed cases
+#' @param i_recovered index indicating recovered people
+#' @param i_dead index indicating the number of dead people
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' as_bass_data(n_covid19$Hubei)
+#'
+as_bass_data <- function(d, location = "Place X", i_date = "Date",
+                         i_confirmed = "Confirmed", i_recovered = "Cured", i_dead = "Dead") {
+
+  if (!is.data.frame(d)) {
+    d <- as.data.frame(d)
+  }
+  d <- d[order(d[i_date]), ]
+
+  cases <- list(
+    Date = d[i_date],
+    len = nrow(d),
+    I = ts(d[i_confirmed]),
+    R = ts(d[i_recovered]),
+    D = ts(d[i_dead])
+  )
+
+  class(cases) <- "BassData"
+  return(cases)
+}
+
+
+#' @rdname fetch_demography
+#' @export
+print.BassData <- function(cases) {
+
+}
